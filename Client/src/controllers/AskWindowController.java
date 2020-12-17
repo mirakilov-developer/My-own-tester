c862fb0b9f65d8760c2c2b9fa0c1925cff66e732
mirakilov-developer <dmirakilov.developer@gmail.com>
2020-12-17 14:59:59 +0500

    Big updates

---
+++ Client/src/controllers/AskWindowController.java
@@ -0,0 +1,100 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import model.Answer;
+import model.Question;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class ClientWindowController {
+    List<String> variants = new ArrayList<>();
+    String correctVariant = "4";
+    private Question que;
+
+    @FXML
+    private ToggleGroup toggleGroup;
+
+    @FXML
+    public Label counter;
+
+    @FXML
+    public Button submit;
+
+    @FXML
+    public Label correctVar;
+
+    @FXML
+    public Label questionText;
+
+    @FXML
+    public RadioButton var1;
+
+    @FXML
+    public RadioButton var2;
+
+    @FXML
+    public RadioButton var3;
+
+    @FXML
+    public RadioButton var4;
+
+
+    @FXML
+    public void initialize() {
+        toggleGroup = new ToggleGroup();
+
+        var1.setToggleGroup(toggleGroup);
+        var2.setToggleGroup(toggleGroup);
+        var3.setToggleGroup(toggleGroup);
+        var4.setToggleGroup(toggleGroup);
+
+        variants.addAll(new ArrayList<String> (List.of("2", "3", "4", "7")));
+        que = new Question("2*2=?", correctVariant, variants);
+        Collections.shuffle(variants);
+
+        questionText.setText("2**2=?");
+        var1.setText(variants.get(0));
+        var2.setText(variants.get(1));
+        var3.setText(variants.get(2));
+        var4.setText(variants.get(3));
+    }
+
+    @FXML
+    public void receiveTests(){
+
+    }
+
+    public void variantClick(MouseEvent mouseEvent) {
+
+    }
+
+    public void sendAnswers(MouseEvent mouseEvent) {
+        if (!var1.isSelected() && !var2.isSelected() &&
+                !var3.isSelected() && var1.isSelected()) {
+            return;
+        }
+
+        String answer = "";
+
+        if (var1.isSelected()) answer = var1.getText();
+        if (var2.isSelected()) answer = var2.getText();
+        if (var3.isSelected()) answer = var3.getText();
+        if (var4.isSelected()) answer = var4.getText();
+
+        Answer ans = new Answer(que, answer);
+
+        if (ans.isCorrect()){
+            correctVar.setText("It's correct");
+        } else correctVar.setText("No, it's not correct, correct variant is " + que.getCorrectVariantStr());
+
+        correctVar.setVisible(true);
+    }
+}
